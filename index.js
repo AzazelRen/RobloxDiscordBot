c7399d2b9a8cfdb837aeae66e6ca7ac7fe9b9bdd
Azazel <azazelren0@gmail.com>
2026-09-17 09:04:17 +0300

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const { Client, GatewayIntentBits } = require("discord.js");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Discord bot is online!");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Web server is online!");
+});
 
 const client = new Client({
     intents: [
